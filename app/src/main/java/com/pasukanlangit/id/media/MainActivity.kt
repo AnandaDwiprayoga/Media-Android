3c066c387e1bd68f26af978ef1c5e0cf6875c930
Ananda Dwi Prayoga <prayogaananda04@gmail.com>
2022-03-14 10:10:13 +0700

    finish media player

--- app/src/main/java/com/pasukanlangit/id/media/MainActivity.kt
+++ app/src/main/java/com/pasukanlangit/id/media/MainActivity.kt
@@ -1,39 +1,65 @@
 package com.pasukanlangit.id.media
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.media.SoundPool
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var sp: SoundPool
-    private var soundId: Int = 0
-    private var spLoaded = false
+    private var mMediaPlayer: MediaPlayer? = null
+    private var isReady: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sp = SoundPool.Builder()
-            .setMaxStreams(10)
+        init()
+    }
+
+    private fun init(){
+        mMediaPlayer = MediaPlayer()
+        val attribute = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
             .build()
 
-        sp.setOnLoadCompleteListener { soundPool, sampleId, status ->
-            if(status == 0){
-                spLoaded = true
-            }else{
-                Toast.makeText(this@MainActivity, "Gagal load", Toast.LENGTH_SHORT).show()
-            }
+        mMediaPlayer?.setAudioAttributes(attribute)
+        val afd = applicationContext.resources.openRawResourceFd(R.raw.sword)
+        try {
+            mMediaPlayer?.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+        }catch(e: IOException){
+            e.printStackTrace()
         }
 
-        soundId = sp.load(this, R.raw.clinking_glasses, 1)
+        mMediaPlayer?.setOnPreparedListener {
+            isReady = true
+            mMediaPlayer?.start()
+        }
+
+        mMediaPlayer?.setOnErrorListener { _, _, _ -> false }
+
     }
 
     fun onPlay(view: View) {
-        if(spLoaded){
-            sp.play(soundId, 1f,1f,0,0,1f)
+        if(!isReady){
+            mMediaPlayer?.prepareAsync()
+        }else{
+            if(mMediaPlayer?.isPlaying == true){
+                mMediaPlayer?.pause()
+            }else{
+                mMediaPlayer?.start()
+            }
+        }
+    }
+    fun onStop(view: View) {
+        if(mMediaPlayer?.isPlaying == true || isReady){
+            mMediaPlayer?.stop()
+            isReady = false
         }
     }
 }
